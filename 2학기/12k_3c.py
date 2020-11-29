9aef976125ea0df5c0b100eb73f1bd5b9f9ab024
dhkeum9886 <dhkeum9886@gmail.com>
2020-11-29 09:03:01 +0900

    * 13주차

--- 2학기/12k_3c.py
+++ 2학기/12k_3c.py
@@ -34,29 +34,30 @@ if __name__ == '__main__':
     # plt.show()
 
 
-    # male = 0
-    # female = 0
-    # # name = input('인구 구조가 알고싶은 지역의 이름(읍면동 단위)를 입력해주세요 :')
-    # name = '신도림동'
-    # with open('gender.csv') as f:
-    #     data = csv.reader(f)
-    #
-    #     for row in data:
-    #         if (name) in row[0]:
-    #             for i in row[3:104]:
-    #                 male += int(i)
-    #             for i in row[106:]:
-    #                 female += int(i)
-    #
-    # print(male, female)
-    #
-    # plt.rc('font', family='Malgun Gothic')
-    # plt.axis('equal')
-    # plt.pie([male, female],
-    #         labels=['남성', '여성'],
-    #         autopct='%.1f%%',
-    #         colors=['royalblue', 'deeppink'],
-    #         startangle=90)      # 시작 각도, 0 의 위치는 3시 방향. 90의 값은 12시 방향
-    # plt.title(name + ' 지역의 남녀 성별 인구 분포')
-    # plt.legend()
-    # plt.show()
+    male = 0
+    female = 0
+    # name = input('인구 구조가 알고싶은 지역의 이름(읍면동 단위)를 입력해주세요 :')
+    name = '신도림동'
+    with open('gender.csv') as f:
+        data = csv.reader(f)
+
+        for row in data:
+            if (name) in row[0]:
+                for i in row[3:104]:
+                    male += int(i)
+                for i in row[106:]:
+                    female += int(i)
+
+    print(male, female)
+
+    plt.rc('font', family='Malgun Gothic')
+    plt.axis('equal')
+    plt.pie([male, female],
+            labels=['남성', '여성'],
+            autopct='%.1f%%',
+            colors=['royalblue', 'deeppink'],
+            startangle=90)      # 시작 각도, 0 의 위치는 3시 방향. 90의 값은 12시 방향
+    plt.title(name + ' 지역의 남녀 성별 인구 분포')
+    plt.legend()
+    plt.savefig(name + ' 지역의 남녀 성별 인구 분포' + '.png')     # 이미지 파일로 저장.
+    plt.show()
